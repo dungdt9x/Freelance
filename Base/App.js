fa8bbf20c0d33242e98f771cacc64dbcff3d17b8
Do Trung Dung <dungdtiosdeveloper@gmail.com>
2022-11-01 00:18:34 +0700

    navigation

--- Base/App.js
+++ Base/App.js
@@ -6,27 +6,36 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useReducer, useState} from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {StatusBar, Text, View} from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
 import AnimatedSplash from 'react-native-animated-splash-screen';
 import {Appbar} from 'react-native-paper';
-import {Storages} from './src/constants/storages';
-import DeviceInfo from 'react-native-device-info';
-import Fonts from './src/constants/fonts';
-import MultipleImagePicker from '@baronha/react-native-multiple-image-picker';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {AuthContext} from './src/api/Authenticate';
+import {createStackNavigator} from '@react-navigation/stack';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
-const HomeScreen = () => {
+const initializeState = {};
+const Stack = createStackNavigator();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'A':
+      return {
+        ...state,
+      };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const insets = useSafeAreaInsets();
   return (
-    <SafeAreaView style={'white'}>
+    <View style={{flex: 1, backgroundColor: 'white', paddingTop: insets.top}}>
       <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
       <Appbar.Header
         style={{
@@ -38,46 +47,67 @@ const HomeScreen = () => {
         <Appbar.Action icon="magnify" onPress={() => {}} />
         <Appbar.Action icon="dots-vertical" onPress={() => {}} />
       </Appbar.Header>
-      <View>
-        <TouchableOpacity
-          style={{
-            paddingVertical: 12,
-            paddingHorizontal: 20,
-            backgroundColor: 'red',
-            alignSelf: 'center',
-            marginTop: 40,
-          }}
-          onPress={() => {
-            MultipleImagePicker.openPicker([
-              {
-                selectedAssets: [],
-                usedCameraButton: true,
-                mediaType: 'image',
-              },
-            ])
-              .then(response => {
-                console.log('Response: ', response);
-              })
-              .catch(error => {
-                console.log('Error: ', error);
-              });
-          }}>
-          <Text
-            style={{fontFamily: Fonts.Regular, color: 'white', fontSize: 13}}>
-            Open Image picker
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+    // <SafeAreaView style={'white'}>
+    //
+    //   <View>
+    //     <TouchableOpacity
+    //       style={{
+    //         paddingVertical: 12,
+    //         paddingHorizontal: 20,
+    //         backgroundColor: 'red',
+    //         alignSelf: 'center',
+    //         marginTop: 40,
+    //       }}
+    //       onPress={() => {
+    //         MultipleImagePicker.openPicker([
+    //           {
+    //             selectedAssets: [],
+    //             usedCameraButton: true,
+    //             mediaType: 'image',
+    //           },
+    //         ])
+    //           .then(response => {
+    //             console.log('Response: ', response);
+    //           })
+    //           .catch(error => {
+    //             console.log('Error: ', error);
+    //           });
+    //       }}>
+    //       <Text
+    //         style={{fontFamily: Fonts.Regular, color: 'white', fontSize: 13}}>
+    //         Open Image picker
+    //       </Text>
+    //     </TouchableOpacity>
+    //   </View>
+    // </SafeAreaView>
   );
 };
 
 const App: () => Node = () => {
   const [ready, setReady] = useState(false);
-
+  const [state, dispatch] = useReducer(reducer, initializeState);
   useEffect(() => {
-    console.log(Storages.getAllKeys());
-    console.log(DeviceInfo.getBundleId());
     setTimeout(() => {
       RNBootSplash.hide({fade: true}).then(() => {
         setReady(true);
@@ -87,18 +117,21 @@ const App: () => Node = () => {
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <AnimatedSplash
-        translucent={true}
-        isLoaded={ready}
-        logoImage={require('./src/assets/icons/sun.png')}
-        backgroundColor={'white'}
-        logoHeight={100}
-        logoWidth={100}>
-        <SafeAreaView style={'white'}>
-          <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />
-          {HomeScreen()}
-        </SafeAreaView>
-      </AnimatedSplash>
+      <NavigationContainer>
+        <AuthContext.Provider value={{state, dispatch}} />
+        <AnimatedSplash
+          translucent={true}
+          isLoaded={ready}
+          logoImage={require('./src/assets/icons/sun.png')}
+          backgroundColor={'white'}
+          logoHeight={100}
+          logoWidth={100}>
+          {/*<SafeAreaView style={'white'}>*/}
+          {/*  <StatusBar barStyle={'dark-content'} backgroundColor={'white'} />*/}
+          {HomeStack()}
+          {/*</SafeAreaView>*/}
+        </AnimatedSplash>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 };
