0266e94fde3a0c9e62f5c571f08d5989fdb6f9dd
dungdt-itealteam <dungdt.iteal@gmail.com>
2023-06-22 09:11:53 +0700

    Update

--- Base/App.js
+++ Base/App.js
@@ -103,7 +103,7 @@ const App: () => Node = () => {
     return () => {
       unsubscribeLoaded();
     };
-  }, []);
+  }, [appOpenAd]);
 
   return (
     <GestureHandlerRootView style={styles.rootView}>
